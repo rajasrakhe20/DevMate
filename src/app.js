64a9ddf9087a623053efed7f089e72f4e3da5d3b
Rajas Rakhe <rajas.rakhe@deeptek.ai>
2025-01-01 15:33:53 +0530

    cookie praser

--- src/app.js
+++ src/app.js
@@ -3,11 +3,34 @@ const app = express();
 
 const {validateSignUpData} = require("./utils/validation");
 const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
 const connectDB = require("./config/database");
 const User = require("./models/user");
 
 app.use(express.json());
+app.use(cookieParser());
+
+
+app.get("/profile", async (req, res) => {
+    try {
+      const cookies = req.cookies;
+      const { token } = cookies;
+      if (!token) {
+        throw new Error("Invalid Token");
+      }
+      const decodedMessage = await jwt.verify(token, "DEV@Mate$790");
+      const { _id } = decodedMessage;
+      const user = await User.findById(_id);
+      if (!user) {
+        throw new Error("User does not exist");
+      }
+      res.send(user);
+    } catch (err) {
+      res.status(400).send("ERROR : " + err.message);
+    }
+  });
 
 
 app.get("/user", async (req,res)=>{
@@ -106,7 +129,12 @@ app.post("/login", async (req, res) => {
       }
       const isPasswordValid = await bcrypt.compare(password, user.password);
       if (isPasswordValid) {
+        // Create a JWT Token
+        const token = await jwt.sign({ _id: user._id }, "DEV@Mate$790");
+       // Add the token to cookie and send the response back to the user
+        res.cookie("token", token);
         res.send("Login Successful!!!");
+
       } else {
         throw new Error("Invalid credentials");
       }
